8ee79c96b61c4426d6ae32be84210a6dfa9b70bc
Kriz-Croz <cruzkhrizjan@gmail.com>
2024-04-23 14:58:15 +0800

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,12 +1,12 @@
-<footer class="bg-dark text-white pt-5 pb-4">
+<footer class="mt-auto bg-dark text-white pt-5 pb-4">
     <div class="container text-center text-md-left">
         <div class="row text-center text-md-left">
             <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
                 <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Foodies</h5>
             </div>
-            <div class="col-md-2 col-lg-2 col-xl-2 mt-3">
+            <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
                 <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Categories</h5>
-                <ul>
+                <ul class="p-0">
                     <?php
                         $cats = $showCategory->showRecords();
                         if(count($cats) > 0){
@@ -21,7 +21,7 @@
                     ?>
                 </ul>
             </div>
-            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+            <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
                 <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Social Links</h5>
                 <p>
                     <a href="" class="text-white" style="text-decoration: none;"><i class="bi bi-facebook"></i></a>
@@ -29,9 +29,9 @@
                     <a href="" class="text-white" style="text-decoration: none;"><i class="bi bi-youtube"></i></a>
                 </p>
             </div>
-            <div class="col-md-4 col-lg-3 col-xl-4 mx-auto-3 mt-3">
+            <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
                 <h5 class="text-uppercase mb-4 font-weight-bold text-warning">Contact</h5>
-                <ul>
+                <ul class="p-0">
                     <li style="list-style: none;">
                         <a href="" class="text-white" style="text-decoration: none;"><i class="bi bi-geo-alt-fill"></i> Lupao, Nueva Ecija</a>
                     </li>
