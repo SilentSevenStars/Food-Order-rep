fc629a0bd83c114e82cff49d270b08b65c40f2cb
Kriz-Croz <cruzkhrizjan@gmail.com>
2024-04-23 15:17:21 +0800

    Revert "Update header.php"
    This reverts commit 5cfeb835b2afe0e868bb1b33ae9300668ab38363.

--- includes/header.php
+++ includes/header.php
@@ -4,29 +4,29 @@
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse pt-2" id="navbarNavDropdown">
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav ms-auto">
-                <li class="nav-item shadow-sm rounded px-2">
+                <li class="nav-item">
                     <a class="nav-link" href="index.php">Home</span></a>
                 </li>
-                <li class="nav-item shadow-sm rounded px-2">
+                <li class="nav-item">
                     <a class="nav-link" href="menu.php">Menu</span></a>
                 </li>
-                <li class="nav-item shadow-sm rounded px-2">
+                <li class="nav-item">
                     <a class="nav-link" href="about.php">About Us</a>
                 </li>
-                <li class="nav-item shadow-sm rounded px-2">
+                <li class="nav-item">
                     <a class="nav-link" href="contact.php">Contact Us</a>
                 </li>
-                <li class="nav-item shadow-sm rounded px-2">
+                <li class="nav-item">
                     <a class="nav-link" href="order.php">Order</a>
                 </li>
       </ul>
       <ul class="navbar-nav ms-auto">
-                <li class="nav-item shadow-sm rounded px-2">
+                <li class="nav-item">
                     <a class="nav-link" href="search.php"><i class="bi bi-search" style="font-size: 18px;"></i></a>
                 </li>
-                <li class="nav-item shadow-sm rounded px-2">
+                <li class="nav-item">
                     <a class="nav-link" href="cart.php"><i class="bi bi-cart" style="font-size: 18px;"></i>[
                         <?php
                             if(isset($_SESSION['cart_id'])){
@@ -48,15 +48,15 @@
                 <?php
                     if(!isset($_SESSION['customer_id'])){
                 ?>
-                <li class="nav-item shadow-sm rounded px-2">
+                <li class="nav-item">
                     <a class="nav-link" href="login.php">Login</a>
                 </li>
                 <?php
                     } else {
                 ?>
-                <div class="nav-item shadow-sm rounded px-2 navbar-collapse navbar">
+                <div class="navbar-collapse navbar">
                     <ul class="navbar-nav">
-                        <li class="dropdown">
+                        <li class="nav-item dropdown">
                             <a href="#" data-bs-toggle="dropdown" class="navbar-brand pe-md-0">
                                 <i class="bi bi-person"></i>
                             </a>
